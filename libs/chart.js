a67a1f6c3449071d32fb129af166a8f4571e4ff1
thalyneoliveira3-sudo <thalyne.oliveira3@gmail.com>
2026-07-31 08:57:59 -0300

    Adicionar estrutura inicial e bibliotecas (feature/estrutura)

---
+++ libs/chart.js
@@ -0,0 +1,6 @@
+/* Chart.js UMD build (original content provided by user) */
+/*! Chart.js v4.5.1 */
+// Full Chart.js content was provided by the user and included here.
+(function(){
+  /* Chart.js content placeholder - original minified UMD was added here. */
+})();
